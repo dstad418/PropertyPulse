1c935878db2c61f5450a4ce285a69521b4099ba8
Ronak <guliani@pdx.edu>
2023-12-01 03:03:52 -0800

    Create context to share selected issue to heatmap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import { FC } from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
+	BrowserRouter as Router,
+	Route,
+	Routes,
+	Navigate,
 } from 'react-router-dom';
 import NavBar from './components/utility-components/NavBar';
 import HomePage from './components/webpages/HomePage';
@@ -11,26 +11,35 @@ import CalendarPage from './components/webpages/CalendarPage';
 import PropertyReport from './components/webpages/PropertyReport';
 import { LightDarkMode } from './components/utility-components/ThemeContext';
 import { FavoriteStarContext } from './components/utility-components/FavoritesContext';
+import { SelectedIssueProvider } from './components/utility-components/SelectedIssueContext.tsx';
 
 const App: FC = () => {
-  return (
-    <Router>
-      <LightDarkMode>
-        <FavoriteStarContext>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-            <Route
-              path="/propertyreport"
-              element={<PropertyReport />}
-            />
-            <Route path="/calendar" element={<CalendarPage />} />
-          </Routes>
-        </FavoriteStarContext>
-      </LightDarkMode>
-    </Router>
-  );
+	return (
+		<Router>
+			<LightDarkMode>
+				<FavoriteStarContext>
+					<SelectedIssueProvider>
+						<NavBar />
+						<Routes>
+							<Route path="/" element={<HomePage />} />
+							<Route
+								path="*"
+								element={<Navigate to="/" replace />}
+							/>
+							<Route
+								path="/propertyreport"
+								element={<PropertyReport />}
+							/>
+							<Route
+								path="/calendar"
+								element={<CalendarPage />}
+							/>
+						</Routes>
+					</SelectedIssueProvider>
+				</FavoriteStarContext>
+			</LightDarkMode>
+		</Router>
+	);
 };
 
 export default App;
