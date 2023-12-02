2fad75a72def2eca309d20cf98f2cedc1f3fd730
Dylan H <dstad41@hotmail.com>
2023-12-01 18:11:36 -0800

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the chunk size warning limit
+    chunkSizeWarningLimit: 1000, // for example, set to 1000 KB
+  },
 })
